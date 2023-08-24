0c00776c0ac22fa76536d058c7271024635760a6
josehigor27 <josehigor27@gmail.com>
2023-08-24 15:40:56 -0300

    feat: initial commit

---
+++ src/main/java/com/springboot/apiurl/ApiUrlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.apiurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/apiurl/ApiUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.apiurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
